496967c3c99e6f4f056a0a5b218ebcd7660af887
Ankit Mahule <ank.mahule@gmail.com>
2023-05-22 05:20:01 +0530

    Added server and database connection.

---
+++ server.mjs
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import "./loadEnvironment.mjs";
+import records from "./routes/record.mjs";
+
+const PORT = process.env.port || 5050;
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/api/v1", records);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
